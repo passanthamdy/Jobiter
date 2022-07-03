15453ba464ba93440c0c8a434ee69ea05a806e09
passanthamdy <passant.hamdy99@gmail.com>
2022-07-03 13:00:51 +0200

    Adding salary. review and skill models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+EMP_STATUS=(
+    ("FULLTIME","Full-Time"),
+    ("PARTTIME","Part-Time"),
+    ("INTERN","Internship"),
 
+)
 # Create your models here.
+class Review(models.Model):
+    company=models.ForeignKey("accounts.User", verbose_name="Company", on_delete=models.CASCADE)
+    reviewer=models.ForeignKey("accounts.User", verbose_name="Reviewer", on_delete=models.CASCADE)
+    pros=models.TextField()
+    cons=models.TextField()
+    employment_status=models.CharField(choices=EMP_STATUS, max_length=50)
+    rating=models.IntegerField()
+    is_published= models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Review no. "+self.id
+    
+
+
--- salaries/models.py
+++ salaries/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Salary(models.Model):
+    company=models.ForeignKey("accounts.User", verbose_name="Company", on_delete=models.CASCADE)
+    reviewer=models.ForeignKey("accounts.User", verbose_name="Reviewer", on_delete=models.CASCADE)
+    salary = models.IntegerField()
+    job_title=models.CharField( max_length=50)
+    start_date= models.DateField()
+    end_date= models.DateField(null=True, default=None)
+    is_published= models.BooleanField(default=False)
+
+
+
+    def __str__(self):
+        return "Salary review no. "+ self.id
+    
+
