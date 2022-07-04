258d32b3c188bb550623582b7a09a7b3e53c5378
Youssef02222 <youssef.ibrahem.2022@gmail.com>
2022-07-04 11:50:03 +0200

    add admin panel for  experience  interviews jobs reviews salaries

--- experience/admin.py
+++ experience/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ['user', 'company_name','job_title','description','start_date', 'end_date', ]
+    search_fields = ('user','company_name','job_title')
