ce84e44ca0891f206470d456f062867e6a3d813b
Youssef02222 <youssef.ibrahem.2022@gmail.com>
2022-07-08 15:07:47 +0200

    solve conflicts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -11,12 +12,15 @@ USER_TYPE = (
 class User(AbstractUser):
     user_type = models.CharField(choices=USER_TYPE, max_length=50, default="DEVELOPER")
     allow_notification = models.BooleanField(default=False)
-    #change to phone field 
     phone=models.CharField(max_length=50,)
     image =models.ImageField(upload_to="user_images/")
 
 
     
 
+
     def __str__(self):
         return self.username + ' ' + self.user_type
+
+    # def __str__(self):
+    #     return str('%s %s ' % (self.phone, self.user_type))
